506122c231f37a845a03291cc6d9d4ddd86ffd56
Taofeek Ibrahim (CodingAlgorithm) <taofeekibrahimope@gmail.com>
2022-09-28 22:12:23 +0100

    Chat app development completed

---
+++ index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoutes");
+const messageRoute = require("./routes/messagesRoute");
+const socket = require("socket.io");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoute);
+app.use("/api/messages", messageRoute);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Sever started on Port ${process.env.PORT} `);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: ["http://localhost:3000", "*"],
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    console.log(userId, {...userId})
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
+});
+ 
